f4b3717827cdeca57659cf0c78e93f6c2de12a2e
Mahender <mahender.j@softlinklocal.com>
2025-02-14 19:27:05 +0530

    chore(api): Add custom exception.

---
+++ CustomExceptions/ModelNotSupportedException.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Happy Solutions.
+// All rights reserved.
+// This code is proprietary and confidential.
+// Unauthorized copying of this file, via any medium, is strictly prohibited.
+
+namespace ChatbotBenchmarkAPI.CustomExceptions
+{
+    /// <summary>
+    /// Exception thrown when a requested model is not supported by the system.
+    /// </summary>
+    public class ModelNotSupportedException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelNotSupportedException"/> class.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public ModelNotSupportedException(string message)
+            : base(message)
+        {
+        }
+    }
+}
